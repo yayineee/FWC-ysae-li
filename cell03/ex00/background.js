7a5998fad853ea58fde05207f0837c09b6249ae0
yayineee <yayinee.li@gmail.com>
2026-09-18 09:00:12 +0700

    parallax

---
+++ cell03/ex00/background.js
@@ -0,0 +1,11 @@
+function addClass() {
+            document.getElementById("example").classList.add("highlight");
+        }
+        function removeClass() {
+            document.getElementById("example").classList.remove("bold");
+        }
+        function toggleClass() {
+            document.getElementById("example").classList.toggle("highlight");
+        }
+
+document.getElementById("demo").style.color = "----";
